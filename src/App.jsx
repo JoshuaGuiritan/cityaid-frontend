335663bf65c5bcde68d7c0aebb226f7f4a973466
Joshua Guiritan <joshuaguiritan05@gmail.com>
2025-07-13 02:45:11 +0800

    m

--- src/App.jsx
+++ src/App.jsx
@@ -135,13 +135,13 @@ function App() {
                         </h1>
                       </div>
                     </div>
-                    <a href={!hospitals.tags["contact:phone"] && hospitals.tags.phone ? "tel:" + String(hospitals.tags.phone) : "tel:" + String(hospitals.tags["contact:phone"])} className={`${!hospitals.tags.phone && !hospitals.tags["contact:phone"] ? "hidden" : "block"}`}>
                       <button
                         className={`w-12 h-12 mr-3 bg-green-500 rounded-md cursor-pointer flex justify-center items-center hover:bg-green-600 transition-colors duration-200 ease-out ${hospitals.tags.phone || hospitals.tags["contact:phone"] ? "opacity-100" : "opacity-40"}`}
+                        onClick={`window.location.href='${!hospitals.tags["contact:phone"] && hospitals.tags.phone ? "tel:" + hospitals.tags.phone : "tel:" + hospitals.tags["contact:phone"]}'`}
+                        disabled={!hospitals.tags["contact:phone"] && !hospitals.tags.phone ? true : false}
                       >
                         <img src="/Images/white-call-logo.png" className="w-8" />
                       </button>
-                    </a>
                   </div>
                 ))}
             </div>
